9b00e536be1dde5ea5c27590d26547aabf3977af
brewchetta <chett.tiller@gmail.com>
2021-01-26 08:49:29 -0500

    context now sorts characters based on initiative

--- src/context/characters-context.js
+++ src/context/characters-context.js
@@ -1,13 +1,16 @@
 import {createContext, useReducer} from "react"
 
 const CharactersContext = createContext()
+const byInitiative = (a,b) => (b.initiative || -10) - (a.initiative || -10)
+
 
 const charactersReducer = (state, action) => {
+
   switch (action.type) {
     case "ADD_CHARACTER":
-      return [...state, action.payload]
+      return [...state, action.payload].sort(byInitiative)
     case "UPDATE_CHARACTER":
-      return state.map(character => character._key === action.payload._key ? action.payload : character)
+      return state.map(character => character._key === action.payload._key ? action.payload : character).sort(byInitiative)
     default:
       throw new Error(`Incorrect use of character reducer: ${action.type}`)
   }
