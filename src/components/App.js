c34fcb74316b4d2e9ea2d816f71d94935bac2c4c
brewchetta <chett.tiller@gmail.com>
2021-01-26 08:34:22 -0500

    added characters provider

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,24 @@
+// STYLES //
 import '../styles/App.css';
 import '../styles/InitiativeCard.css';
+// COMPONENTS //
 import InitiativeList from './initiative-list/InitiativeList'
 import DetailContainer from './detail-modal/DetailContainer'
 import SearchContainer from './monster-search/SearchContainer'
+// CONTEXT //
+import CharactersProvider from "../context/characters-context"
 
 function App() {
 
   return (
     <div className="App">
+      <CharactersProvider>
 
-      <InitiativeList />
-      <DetailContainer />
-      <SearchContainer />
+        <InitiativeList />
+        <DetailContainer />
+        <SearchContainer />
 
+      </CharactersProvider>
     </div>
   );
 }
