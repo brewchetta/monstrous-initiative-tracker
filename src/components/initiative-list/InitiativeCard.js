05594b0760a062503781d6171d4a671dc0a10eba
brewchetta <chett.tiller@gmail.com>
2021-01-27 08:47:25 -0500

    can change hit points directly from card

--- src/components/initiative-list/InitiativeCard.js
+++ src/components/initiative-list/InitiativeCard.js
@@ -1,4 +1,4 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { DetailContext } from '../../context/detail-context'
 import { CharactersContext } from '../../context/characters-context'
 
@@ -6,6 +6,8 @@ export default function InitiativeCard({character, index, updateCharacter}) {
 
   const [detail, dispatchDetail] = useContext(DetailContext)
   const [characters, dispatchCharacters] = useContext(CharactersContext)
+  const [hitPointsOpen, setHitPointsOpen] = useState(false)
+  const [currentHitPoints, setCurrentHitPoints] = useState(character.hit_points)
 
   const handleRollInitiative = () => {
     character.rollInitiative()
@@ -22,13 +24,41 @@ export default function InitiativeCard({character, index, updateCharacter}) {
     dispatchCharacters({type: "REMOVE_CHARACTER", payload: character})
   }
 
+  const handleSetHitPoints = e => {
+    const newHP = parseInt(e.target.value)
+    if (newHP > 0) {
+      setCurrentHitPoints(newHP)
+    } else {
+      console.log("remove character")
+    }
+  }
+
+  const renderHitPoints = () => {
+    if (hitPointsOpen) {
+      return (
+        <span>
+          <input type="number"
+          onChange={e => setCurrentHitPoints(e.target.value)}
+          onBlur={() => setHitPointsOpen(false)}
+          value={currentHitPoints}
+          placeholder={"hit points"}
+          />
+          /{character.hit_points}
+        </span>
+      )
+    } else {
+      return (<span onClick={() => setHitPointsOpen(true)}>{`${currentHitPoints}/${character.hit_points} ♡`}</span>)
+    }
+    console.log(currentHitPoints)
+  }
+
   return (
     <div className="initiative-card"
     style={{top: `${index * 50}px`}}>
       {character.name} | {character.armor_class} 🛡️ | <span
       onClick={handleRollInitiative}>
         {character.initiative >= -4 ? character.initiative : "⚅ Roll for "} Initiative
-      </span> | {character.currentHitPoints}/{character.hit_points} ♡ | <span onClick={handleOpenDetail}>📖 More</span> | <span onClick={handleRemove}>Delete</span>
+      </span> | {renderHitPoints()} | <span onClick={handleOpenDetail}>📖 More</span> | <span onClick={handleRemove}>Delete</span>
     </div>
   )
 
