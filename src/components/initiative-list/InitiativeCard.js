7aafb8f49046dae48e5a2763ffbb8da88141f66b
brewchetta <chett.tiller@gmail.com>
2021-01-27 12:38:48 -0500

    inititiave now persists when changed in card

--- src/components/initiative-list/InitiativeCard.js
+++ src/components/initiative-list/InitiativeCard.js
@@ -27,6 +27,7 @@ export default function InitiativeCard({character, index, updateCharacter}) {
 
   const handleRollInitiative = () => {
     if (character.initiative > -5) {
+      setInitiativeInput(character.initiative)
       setInitiativeOpen(true)
     } else {
       const init = character.rollInitiative()
@@ -55,7 +56,8 @@ export default function InitiativeCard({character, index, updateCharacter}) {
 
   const handleCloseInitiative = () => {
     setInitiativeOpen(false)
-    // TODO: update character initiative
+    character.initiative = parseInt(initiativeInput)
+    updateCharacter(character)
   }
 
   // RENDERS //
