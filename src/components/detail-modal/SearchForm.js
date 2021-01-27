cbc34fc43f46033a5cf225553d956542eb98240d
brewchetta <chett.tiller@gmail.com>
2021-01-26 21:30:54 -0500

    search form tweaks

--- src/components/detail-modal/SearchForm.js
+++ src/components/detail-modal/SearchForm.js
@@ -1,9 +1,30 @@
 import { useState } from "react"
+import Character from '../../models/Character.js'
 
 export default function SearchForm() {
 
+  const [input, setInput] = useState("")
+  const [mode, setMode] = useState("Monster")
+
+  // TODO: add mode select for spells
+
+  const handleSubmit = (event) => {
+    console.log(`Find ${mode} firing`)
+  }
+
   return (
-    
+    <form id="search-form" onSubmit={handleSubmit}>
+
+      <label>Find a {mode}</label>
+      <input type="text"
+      onChange={(e) => setInput(e.target.value)}
+      value={input}
+      placeholder={search here}/>
+
+      <input type="submit"
+      value={Search}/>
+
+    </form>
   )
 
 }
