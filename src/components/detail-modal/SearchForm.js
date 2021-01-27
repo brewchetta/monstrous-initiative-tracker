03693fe0cf327f8af05e287f08829432224a7111
brewchetta <chett.tiller@gmail.com>
2021-01-26 21:41:59 -0500

    search form can fetch monsters

--- src/components/detail-modal/SearchForm.js
+++ src/components/detail-modal/SearchForm.js
@@ -1,19 +1,31 @@
-import { useState } from "react"
+import { useState, useContext } from "react"
 import Character from '../../models/Character.js'
+import { CharactersContext } from "../../context/characters-context"
 
 export default function SearchForm() {
 
   const [input, setInput] = useState("")
   const [mode, setMode] = useState("Monster")
+  const [message, setMessage] = useState("")
+  const [characters, dispatch] = useContext(CharactersContext)
 
   // TODO: add mode select for spells
 
-  const handleSubmit = (event) => {
-    console.log(`Find ${mode} firing`)
+  const handleSubmit = e => {
+    e.preventDefault()
+    Character.search(input).then(character => {
+      if (character) {
+        dispatch({type: "ADD_CHARACTER", payload: character})
+      } else {
+        setMessage("Couldn't find that in the monster manual")
+      }
+    })
   }
 
   return (
     <form id="search-form" onSubmit={handleSubmit}>
+
+      <p>{message}</p>
 
       <label>Find a {mode}</label>
       <input type="text"
