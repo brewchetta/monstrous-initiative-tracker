d1875415b9cdec130c0eda319d760bc9a8d8757b
brewchetta <chett.tiller@gmail.com>
2021-01-28 12:59:59 -0500

    moved add action to button to avoid adding and removing at the same time

--- src/components/detail-modal/SelectLocalMonstersCard.js
+++ src/components/detail-modal/SelectLocalMonstersCard.js
@@ -5,10 +5,10 @@ export default function SelectLocalMonstersCard({character, removeLocalMonster, 
   }
 
   return (
-    <div className="local-character-card"
-    onClick={handleAddMonster}>
-      {character.name}{character.char_class ? ` the ${character.char_class}` : ""}
+    <div className="local-character-card">
+      <button onClick={handleAddMonster}>+</button>
       <button onClick={() => removeLocalMonster(character)}>Remove</button>
+      {character.name}{character.char_class ? ` the ${character.char_class}` : ""}
     </div>
   )
 }
