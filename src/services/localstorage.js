7f8c397d76b49d49c1ecabb229c9bc7b5a9af4c7
brewchetta <chett.tiller@gmail.com>
2021-01-28 20:59:39 -0500

    removed console.logs

--- src/services/localstorage.js
+++ src/services/localstorage.js
@@ -2,9 +2,6 @@ export const getMonsters = () => JSON.parse(localStorage.getItem("monstrous-char
 export const setMonsters = locals => localStorage.setItem("monstrous-characters", JSON.stringify(locals))
 export const clearMonsters = () => localStorage.removeItem("monstrous-characters")
 export const saveMonster = local => {
-    console.log(local)
     const locals = getMonsters() ? [...getMonsters(), local] : [local]
-    console.log(locals)
     setMonsters(locals)
-    console.log(getMonsters())
 }
