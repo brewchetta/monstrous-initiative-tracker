c0c9c86944a6899cfd8a6d4be5e0a99d2a2984e1
brewchetta <chett.tiller@gmail.com>
2021-01-30 16:41:04 -0500

    spells now render correctly

--- src/components/detail-modal/CharacterDetailSpellsList.js
+++ src/components/detail-modal/CharacterDetailSpellsList.js
@@ -1,15 +1,19 @@
 export default function CharacterDetailSpellsList({spells, spell_dc, spell_modifier}) {
 
   const renderSpells = () => {
-    spells.sort((a,b) => b.level - a.level).map(sp => `${sp.name} | Level ${sp.level}`)
+    return (
+      spells
+      .sort((a,b) => a.level - b.level)
+      .map(sp => <><span key={sp.name}>{sp.name} | Level {sp.level}</span><br/></>)
+    )
   }
 
   return (
     <div>
 
-      <span>Spell Modifier: {spell_modifier > 0 ? `+${spell_modifier}` : spell_modifier}</span>
+      <span>Spell Modifier: {spell_modifier > 0 ? `+${spell_modifier}` : spell_modifier}</span> <span>Spell DC: {spell_dc}</span>
 
-      <span>Spell DC: {spell_dc}</span>
+      <br/>
 
       {renderSpells()}
 
