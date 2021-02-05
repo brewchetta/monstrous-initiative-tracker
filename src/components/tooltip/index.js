02c2dc6d0f00ccfcae0272993f8a54bf0b42cbd9
brewchetta <chett.tiller@gmail.com>
2021-02-05 16:14:38 -0500

    tooltip repositioned in relation to mouse

--- src/components/tooltip/index.js
+++ src/components/tooltip/index.js
@@ -7,12 +7,15 @@ export default function Tooltip() {
   const [tooltip] = useContext(TooltipContext)
 
   if (tooltip.type === "spell") {
-    console.log(tooltip.position.x);
+
+    const width  = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth
+    const height = window.innerHeight|| document.documentElement.clientHeight|| document.body.clientHeight
+
     return (
       <div id="spell-tooltip"
-        style={{top: `${tooltip.position.y + 10}px`, left: `${tooltip.position.x + 10}px`}}>
+        style={{bottom: `${height - tooltip.position.y}px`, right: `${width - tooltip.position.x + 10}px`}}>
 
-        <p>{tooltip.content} {tooltip.position.x}</p>
+        <p>{tooltip.content}</p>
 
       </div>
     )
