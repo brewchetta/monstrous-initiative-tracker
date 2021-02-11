7e763c6656893cc71f04134643f2d197bad175af
brewchetta <chett.tiller@gmail.com>
2021-02-11 09:01:37 -0500

    moved spell details over to its own component

---
+++ src/components/tooltip/SpellDetailsView.js
@@ -0,0 +1,21 @@
+export default function SpellDetailsView({spellDetails}) {
+
+  return (
+    <>
+
+    {spellDetails.error ? <p>"Unable to fetch that spell, sorry!"</p> : null}
+    {spellDetails.name ? <p>{spellDetails.name}</p> : null}
+    <p>
+      {<span>{spellDetails.level ? `Level ${spellDetails.level}` : "Cantrip"}</span>}{spellDetails.casting_time ? <span>  |  Casting Time: {spellDetails.casting_time}</span> : null}{spellDetails.duration ? <span>  |  Duration: {spellDetails.duration}</span> : null}
+    </p>
+    <p>{spellDetails.concentration ? <span>Concentration</span> : null}</p>
+    <p>
+      {spellDetails.components ? <span>Components: {spellDetails.components.join(" ")}</span> : null}{spellDetails.material ? <span> |  Materials: {spellDetails.material}</span> : null}{spellDetails.ritual ? <span>  |  Can be cast as a ritual</span> : null}
+    </p>
+    {spellDetails.desc ? <p>{spellDetails.desc}</p> : null}
+    {spellDetails.school ? <span>School: {spellDetails.school.name}</span> : null}
+
+    </>
+  )
+
+}
--- src/components/tooltip/index.js
+++ src/components/tooltip/index.js
@@ -1,5 +1,6 @@
 import { useContext, useState, useEffect } from "react"
 import { getSpell } from "../../services/dnd-5e-api"
+import SpellDetailsView from "./SpellDetailsView"
 import { TooltipContext } from "../../context/tooltip-context"
 import "./style.css"
 
@@ -27,17 +28,7 @@ export default function Tooltip() {
           display: spellDetails.name || spellDetails.error ? "block" : "none"
         }}>
 
-        {spellDetails.error ? <p>"Unable to fetch that spell, sorry!"</p> : null}
-        {spellDetails.name ? <p>{spellDetails.name}</p> : null}
-        <p>
-          {<span>{spellDetails.level ? `Level ${spellDetails.level}` : "Cantrip"}</span>}{spellDetails.casting_time ? <span>  |  Casting Time: {spellDetails.casting_time}</span> : null}{spellDetails.duration ? <span>  |  Duration: {spellDetails.duration}</span> : null}
-        </p>
-        <p>{spellDetails.concentration ? <span>Concentration</span> : null}</p>
-        <p>
-          {spellDetails.components ? <span>Components: {spellDetails.components.join(" ")}</span> : null}{spellDetails.material ? <span> |  Materials: {spellDetails.material}</span> : null}{spellDetails.ritual ? <span>  |  Can be cast as a ritual</span> : null}
-        </p>
-        {spellDetails.desc ? <p>{spellDetails.desc}</p> : null}
-        {spellDetails.school ? <span>School: {spellDetails.school.name}</span> : null}
+        <SpellDetailsView spellDetails={spellDetails} />
 
       </div>
     )
