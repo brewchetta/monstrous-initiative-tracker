db9dd6cd5ef3058834a09df7761e443eabc443f5
brewchetta <chett.tiller@gmail.com>
2021-02-25 18:04:01 -0500

    now properly utilizes a form and allows for submissions

--- src/components/character-detail/AddSpellForm.js
+++ src/components/character-detail/AddSpellForm.js
@@ -17,7 +17,7 @@ export default function AddSpellForm({handleSubmit, spellNames, spells}) {
   const handleLevelChange = e => setLevelInput(e.target.value)
 
   const handleSubmitForm = e => {
-    console.log("Doing stuff")
+    e.preventDefault()
     if (!spells.find(sp => sp.name.toLowerCase() === nameInput.toLowerCase())) {
       handleSubmit({name: nameInput, level: parseInt(levelInput)})
       setNameInput("")
@@ -28,7 +28,7 @@ export default function AddSpellForm({handleSubmit, spellNames, spells}) {
 
   return (
     <>
-    <div id="add-spell-form" onSubmit={handleSubmitForm}>
+    <form id="add-spell-form" onSubmit={handleSubmitForm}>
       <input type="text"
       list="spell-names"
       onChange={handleNameChange}
@@ -53,9 +53,9 @@ export default function AddSpellForm({handleSubmit, spellNames, spells}) {
         <option value={9}>9</option>
       </select>
 
-      <input type="button"
-      value={"Add Spell"}/>
-    </div>
+      <input type="submit"
+      value="Add Spell"/>
+    </form>
     </>
   )
 
