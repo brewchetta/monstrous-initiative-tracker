eccfcf7f82f5f394cb8fcec2b587f4ba2c123b82
brewchetta <chett.tiller@gmail.com>
2021-02-25 18:35:37 -0500

    cantrip shouldn't randomly appear on the spell details sheet anymore

--- src/components/tooltip/SpellDetailsView.js
+++ src/components/tooltip/SpellDetailsView.js
@@ -6,7 +6,7 @@ export default function SpellDetailsView({spellDetails}) {
     {spellDetails.error ? <p>"Unable to fetch that spell, sorry!"</p> : null}
     {spellDetails.name ? <p>{spellDetails.name}</p> : null}
     <p>
-      {<span>{spellDetails.level ? `Level ${spellDetails.level}` : "Cantrip"}</span>}{spellDetails.casting_time ? <span>  |  Casting Time: {spellDetails.casting_time}</span> : null}{spellDetails.duration ? <span>  |  Duration: {spellDetails.duration}</span> : null}
+      {<span>{spellDetails.level ? `Level ${spellDetails.level}` : spellDetails.level === 0 && "Cantrip"}</span>}{spellDetails.casting_time ? <span>  |  Casting Time: {spellDetails.casting_time}</span> : null}{spellDetails.duration ? <span>  |  Duration: {spellDetails.duration}</span> : null}
     </p>
     <p>{spellDetails.concentration ? <span>Concentration</span> : null}</p>
     <p>
