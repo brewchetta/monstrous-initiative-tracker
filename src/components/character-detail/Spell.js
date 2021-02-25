0746159124610b4808e16591aead78d9ccaa3250
brewchetta <chett.tiller@gmail.com>
2021-02-25 16:53:17 -0500

    added remove spell button

--- src/components/character-detail/Spell.js
+++ src/components/character-detail/Spell.js
@@ -1,21 +1,37 @@
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { TooltipContext } from "../../context/tooltip-context"
 
 export default function Spell({spell}) {
 
+  // STATE //
+
+  const [hovered, setHovered] = useState(false)
+
   const dispatch = useContext(TooltipContext)[1]
+
+  // EVENT HANDLERS //
 
   const handleMouseEnter = (e, spellName) => {
     dispatch({type: "INSPECT_SPELL", payload: {spellName, x: e.clientX, y: e.clientY}})
+    setHovered(true)
   }
 
-  const handleMouseLeave = () => dispatch({type: "CLEAR_TOOLTIP"})
+  const handleMouseLeave = () => {
+    dispatch({type: "CLEAR_TOOLTIP"})
+    setHovered(false)
+  }
+
+  const button = <button className="remove-spell-button" onClick={() => console.log("remove spell")}>
+                  {hovered ? 'X' : ''}
+                </button>
+
+  // RENDER //
 
   return (
     <span className="spell-span"
     onMouseEnter={event => handleMouseEnter(event, spell.name)}
     onMouseLeave={handleMouseLeave}
-    key={spell.name}>{spell.name}</span>
+    key={spell.name}>{spell.name} {button}</span>
   )
 
 }
