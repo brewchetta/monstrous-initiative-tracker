b03b5a728b69fca9efaec76f0659131bb8c08ba0
brewchetta <chett.tiller@gmail.com>
2021-05-01 12:27:16 -0400

    fixed issue with blur not properly getting target

--- src/components/character-detail/CharacterTextArea.js
+++ src/components/character-detail/CharacterTextArea.js
@@ -2,7 +2,7 @@ import { useState, useRef, useEffect } from "react"
 
 export default function CharacterTextArea({name, value, handleChange, width, height}) {
 
-  const handleKeyPress = (e) => e.key === "Enter" && e.blur()
+  const handleKeyPress = (e) => e.key === "Enter" && e.target.blur()
 
   return <span style={{width: "100%"}} className="character-detail-field text-area-field">
     <textarea onChange={handleChange}
