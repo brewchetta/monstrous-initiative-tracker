9b41e9b287197a759236fbc532cf5afbabe5294c
brewchetta <chett.tiller@gmail.com>
2021-05-01 12:24:29 -0400

    fixed text areas and inputs to use proper form inputs

--- src/components/character-detail/CharacterTextArea.js
+++ src/components/character-detail/CharacterTextArea.js
@@ -2,30 +2,15 @@ import { useState, useRef, useEffect } from "react"
 
 export default function CharacterTextArea({name, value, handleChange, width, height}) {
 
-  const [focused, setFocused] = useState(false)
-  const focusedInput = useRef(null)
+  const handleKeyPress = (e) => e.key === "Enter" && e.blur()
 
-  useEffect(() => {
-    focused && focusedInput.current.focus()
-  }, [focused]);
-
-  const handleClick = () => !focused && setFocused(true)
-
-  const handleKeyPress = ({key}) => key === "Enter" && setFocused(false)
-
-  const renderArea = () => (
+  return <span style={{width: "100%"}} className="character-detail-field text-area-field">
     <textarea onChange={handleChange}
-    style={{width, height}}
-    name={name}
-    value={value}
-    ref={focusedInput}
-    onKeyUp={handleKeyPress}
-    onBlur={() => setFocused(!focused)}
-    placeholder={name.replace("_"," ")}/>
-  )
-
-  return <span style={{width: "100%"}} onClick={handleClick} className="character-detail-field text-area-field">
-    {focused ? renderArea() : (value || `none`)}
+      style={{width, height}}
+      name={name}
+      value={value}
+      onKeyUp={handleKeyPress}
+      placeholder={name.replace("_"," ")}/>
   </span>
 
 }
--- src/components/character-detail/CharacterTextInput.js
+++ src/components/character-detail/CharacterTextInput.js
@@ -2,27 +2,12 @@ import { useState, useRef, useEffect } from "react"
 
 export default function CharacterTextInput({name, value, handleChange, width}) {
 
-  const [focused, setFocused] = useState(false)
-  const focusedInput = useRef(null)
-
-  useEffect(() => {
-    focused && focusedInput.current.focus()
-  }, [focused]);
-
-  const handleClick = () => !focused && setFocused(true)
-
-  const renderArea = () => (
-    <input type="text"
+  return <span style={{width}} className="character-detail-field text-input">
+  <input type="text"
     onChange={handleChange}
     name={name}
     value={value}
-    ref={focusedInput}
     placeholder={name.replace("_"," ")}/>
-  )
-  // onBlur={() => setFocused(!focused)}
-
-  return <span onClick={handleClick} style={{width}} className="character-detail-field text-input">
-    {focused ? renderArea() : (value || `+`)}
   </span>
 
 }
