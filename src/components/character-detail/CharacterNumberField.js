7076151073badf1790651ba7b8a1b7b140e36269
brewchetta <chett.tiller@gmail.com>
2021-05-01 12:18:04 -0400

    changed number fields to be proper form inputs

--- src/components/character-detail/CharacterNumberField.js
+++ src/components/character-detail/CharacterNumberField.js
@@ -2,28 +2,13 @@ import { useState, useRef, useEffect } from "react"
 
 export default function CharacterNumberField({name, value, handleChange}) {
 
-  const [focused, setFocused] = useState(false)
-  const focusedInput = useRef(null)
-
-  useEffect(() => {
-    focused && focusedInput.current.focus()
-  }, [focused]);
-
-  const handleClick = () => !focused && setFocused(true)
-
-  const renderInput = () => (
-    <input type="number"
-    onChange={handleChange}
-    name={name}
-    value={value}
-    ref={focusedInput}
-    onBlur={() => setFocused(!focused)}
-    placeholder={name.replace("_"," ")}/>
-  )
-
   return (
-    <span onClick={handleClick} className="character-detail-field number-field">
-      {focused ? renderInput() : (value || 0)}
+    <span className="character-detail-field number-field">
+      <input type="number"
+        onChange={handleChange}
+        name={name}
+        value={value}
+        placeholder={name.replace("_"," ")}/>
     </span>
   )
 
