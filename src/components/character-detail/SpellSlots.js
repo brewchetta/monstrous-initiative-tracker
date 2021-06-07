f16e83b97bdb0c90ecd026f829938cb10c7f4dd9
brewchetta <chett.tiller@gmail.com>
2021-06-07 17:09:57 -0400

    initial code for spell slots

--- src/components/character-detail/SpellSlots.js
+++ src/components/character-detail/SpellSlots.js
@@ -1,15 +1,36 @@
-export default function SpellSlots({spellSlots, addSlots, removeSlot}) {
+export default function SpellSlots({spellSlots, addSlots, removeSlot, setSpellSlots}) {
   // recieves a slots prop (array) as well as an add and a remove slots prop
+  // additional prop to set the spell slots
   // example slots prop: [true, true, false, false] for two used slots
   // slots are per level
 
+  const handleClickSpellSlot = (spell, i) => {
+    const slots = [...spellSlots]
+    slots[i] = !spell
+    setSpellSlots(slots)
+  }
+
+  const renderSlots = () => {
+    return spellSlots.map((spell, i) => (
+      <input type="checkbox" checked={spell} onChange={() => handleClickSpellSlot(spell, i)}/>
+    ))
+  }
+
   return (
+    <div id="">
 
-    // conditionally render minus button (must already include spell slots)
+      // conditionally render minus button (must already include spell slots)
+      <button class={spellSlots.length > 0 ? "active slot-button" : "slot-button"} onClick={handleRemoveSlot}>-</button>
 
-    // render spell slots with checkbox for each slot
+      // render spell slots with checkbox for each slot
+      {renderSlots()}
 
-    // conditionally render plus button (up to six slots)
+      // conditionally render plus button (up to six slots)
+      <button class={spellSlots.length < 6 ? "active slot-button" : "slot-button"} onClick={handleRemoveSlot}>-</button>
+
+    </div>
+
+
 
   )
 
