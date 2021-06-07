5983490d3375e7cd2eab4885b3bc47ca54f197f6
brewchetta <chett.tiller@gmail.com>
2021-06-07 16:54:28 -0400

    initial work on spell slots

---
+++ src/components/character-detail/SpellSlots.js
@@ -0,0 +1,13 @@
+export default function SpellSlots({spellSlots, addSlots, removeSlot}) {
+  // recieves a slots prop (array) as well as an add and a remove slots prop
+
+  return (
+
+    // conditionally render plus and minus buttons (up to six slots)
+
+  )
+
+  // has a minus on one end which removes the slots and a plus on the other that increases
+  // slots are checkboxes which persist for that character
+
+}
