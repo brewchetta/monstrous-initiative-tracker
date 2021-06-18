95c0edeb00c7742be26a26466217789bc528b4d2
brewchetta <chett.tiller@gmail.com>
2021-06-18 14:08:33 -0400

    options persist in local storage

--- src/context/options-settings-context.js
+++ src/context/options-settings-context.js
@@ -1,12 +1,24 @@
 import {createContext, useContext, useState} from "react"
+import localStorageService from 'services/localstorage'
+
+const [getLocOptions, setLocOptions] = localStorageService('monstrous-options')
 
 const OptionsSettingsContext = createContext()
 
 export const OptionsSettingsProvider = ({children}) => {
-  const [options, setOptions] = useState({preRolledInitiative: false, randomHitpoints: true})
+  const [options, setOptions] = useState(getLocOptions() ? getLocOptions() : {preRolledInitiative: false, randomHitpoints: true})
 
-  const togglePreRolledInitiative = () => setOptions({...options, preRolledInitiative: !options.preRolledInitiative})
-  const toggleRandomHitpoints = () => setOptions({...options, randomHitpoints: !options.randomHitpoints})
+  const togglePreRolledInitiative = () => {
+    const newSettings = {...options, preRolledInitiative: !options.preRolledInitiative}
+    setOptions(newSettings)
+    setLocOptions(newSettings)
+  }
+
+  const toggleRandomHitpoints = () => {
+    const newSettings = {...options, randomHitpoints: !options.randomHitpoints}
+    setOptions(newSettings)
+    setLocOptions(newSettings)
+  }
 
   return (
     <OptionsSettingsContext.Provider value={{options, toggleRandomHitpoints, togglePreRolledInitiative}}>
