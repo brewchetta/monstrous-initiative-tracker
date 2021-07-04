536cd2834cfd0496b7dda9a2a865b12820839ace
brewchetta <chett.tiller@gmail.com>
2021-07-04 09:58:18 -0400

    changed spells context to properly be an object

--- src/context/spells-context.js
+++ src/context/spells-context.js
@@ -3,7 +3,7 @@ import {createContext, useContext, useState} from "react"
 const SpellsContext = createContext()
 
 export const SpellsProvider = ({children}) => {
-  const [all, setSpells] = useState([])
+  const [all, setSpells] = useState({})
 
   const clear = () => setSpells([])
   const addSpell = spell => setSpells(spell.name ? {...all, [spell.name.toLowerCase()]: spell} : all)
